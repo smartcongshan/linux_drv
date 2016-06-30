45bd599e4b02241dd72bc8b258dd4cb0c492839e
smartcongshan <1043503402@qq.com>
2016-06-30 16:04:18 +0800

    2016.6.30

---
+++ 15th_drv/nor.c
@@ -0,0 +1,74 @@
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/device.h>
+#include <linux/platform_device.h>
+#include <linux/mtd/mtd.h>
+#include <linux/mtd/map.h>
+#include <linux/mtd/partitions.h>
+#include <linux/mtd/physmap.h>
+#include <asm/io.h>
+
+static struct map_info *s3c_nor_map;
+static struct mtd_info *s3c_nor_mtd;
+
+static struct mtd_partition s3c_nor_partitions[] = {
+	[0] = {
+        .name   = "bootloader",
+        .size   = 0x00040000,
+		.offset	= 0,
+	},
+	[1] = {
+        .name   = "root_nor",
+        .offset = MTDPART_OFS_APPEND,
+        .size   = MTDPART_SIZ_FULL,
+	}
+};
+
+static int s3c_nor_init(void)
+{
+	/* 分配map_info结构体 */
+	s3c_nor_map = kzalloc(sizeof(struct map_info), GFP_KERNEL);
+	/* 设置map_info结构体 */
+	s3c_nor_map->name = "s3c_nor";
+	s3c_nor_map->phys = 0;			//物理地址
+	s3c_nor_map->size = 0x1000000;	// >= norflash真正的物理地址
+	s3c_nor_map->bankwidth = 2;		//位宽
+	
+	s3c_nor_map->virt = ioremap(s3c_nor_map->phys, s3c_nor_map->size);
+
+	simple_map_init(s3c_nor_map);
+
+	/* 识别norflash */
+	printk("use cfi_probe\n");
+	s3c_nor_mtd = do_map_probe("cfi_probe", s3c_nor_map);
+	if(!s3c_nor_mtd)
+	{
+		printk("use jedec_probe\n");
+		s3c_nor_mtd = do_map_probe("jedec_probe", s3c_nor_map);	
+	}
+
+	if(!s3c_nor_mtd)
+	{
+		iounmap(s3c_nor_map->virt);
+		kfree(s3c_nor_map);
+		return -EIO;
+	}
+	/* add_mtd_partitions */
+	add_mtd_partitions(s3c_nor_mtd,s3c_nor_partitions, 2);
+	return 0;
+}
+
+static void s3c_nor_exit(void)
+{
+		del_mtd_partitions(s3c_nor_mtd);
+		iounmap(s3c_nor_map->virt);
+		kfree(s3c_nor_map);
+
+}
+
+module_init(s3c_nor_init);
+module_exit(s3c_nor_exit);
+
